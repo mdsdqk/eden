9a3fc6284bef03cdff00ca9e79988a28cb72a276
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-10-13 02:18:57 +0530

    update app information add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': "#664de5"
+      }
+    },
   },
   plugins: [],
 };
